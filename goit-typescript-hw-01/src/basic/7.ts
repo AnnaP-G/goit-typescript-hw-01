a19f32859e902810fecd5bf71aa1bb89583c82cd
AnnaP-G <klientkv1511@gmail.com>
2024-05-08 21:44:04 +0300

    homework1-ts

--- goit-typescript-hw-01/src/basic/7.ts
+++ goit-typescript-hw-01/src/basic/7.ts
@@ -9,7 +9,7 @@ interface Pages {
     }
 }
 
-const page1 = {
+const page1: Pages = {
     title: 'The awesome page',
     likes: 100,
     accounts: ['Max', 'Anton', 'Nikita'],
@@ -20,7 +20,7 @@ const page1 = {
     }
 }
 
-const page2 = {
+const page2: Pages = {
     title: 'Python or Js',
     likes: 5,
     accounts: ['Alex'],
